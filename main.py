1c781a3d5c4ab3ddeb6682bf852d444f75af9050
nathannrss <natans751@gmail.com>
2025-03-27 15:20:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 # Disciplina: DevOps
 #Fluxo de CI/CD
 
+#importacao das bibliotecas
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayaout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+#
+
